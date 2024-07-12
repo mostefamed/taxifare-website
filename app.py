0966b00755ab9e0d8899c09539ed525a49426dbf
mostefa medjahed <mostefa.medjahed.pro@gmail.com>
2024-07-12 23:15:24 +0200

    another version

--- app.py
+++ app.py
@@ -3,148 +3,133 @@ import requests
 import urllib.parse
 import datetime
 import folium
-from folium.plugins import Draw, MeasureControl
 from streamlit_folium import st_folium
-import numpy as np
-
-def initialize_map(center, zoom):
-    m = folium.Map(location=center, zoom_start=zoom, scrollWheelZoom=False)
-    draw = Draw(export=False,
-                filename='custom_drawn_polygons.geojson',
-                position='topright',
-                draw_options={'polyline': False,  # disable polyline option
-                              'rectangle': False,  # disable rectangle option for now
-                              # enable polygon option
-                              #   'polygon': {'showArea': True, 'showLength': False, 'metric': False, 'feet': False},
-                              'polygon': False,  # disable rectangle option for now
-                              # enable circle option
-                              'circle': {'showArea': True, 'showLength': False, 'metric': False, 'feet': False},
-                              'circlemarker': False,  # disable circle marker option
-                              'marker': False,  # disable marker option
-                              },
-                edit_options={'poly': {'allowIntersection': False}})
-    draw.add_to(m)
-    MeasureControl(position='bottomleft', primary_length_unit='miles',
-                   secondary_length_unit='meters', primary_area_unit='sqmiles', secondary_area_unit=np.nan).add_to(m)
-    return m
-
-
 
 TAXI_FARE_API_URL = 'https://taxifare.lewagon.ai/predict'
 GEO_LOCALISATION = "https://api-adresse.data.gouv.fr/search/"
-
-
-# Set up initial map state
-CENTER_START = [37.8, -96]
 ZOOM_START = 5
 
-st.header('Bienvenue chez RideEstimate 🛣️', divider='grey')
-st.write("Ne laissez pas l'incertitude vous ralentir, RideEstimate trace votre chemin 🚕")
+def css_style():
+    st.markdown(
+        """
+            <style>
+                p {
+                    font-weight: bold;
+                }
+            </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
-one_ride, many_rides = st.tabs(["Une course", "Plusieurs courses"])
+def header():
+    st.header('Bienvenue chez RideEstimate 🛣️', divider='grey')
+    st.write("Ne laissez pas l'incertitude vous ralentir, RideEstimate trace votre chemin 🚕")
 
-st.markdown(
-    """
-    <style>
-    p {
-        font-weight: bold;
+def tabs():
+    one_ride, many_rides = st.tabs(["Une course", "Plusieurs courses"])
+
+    return one_ride, many_rides
+
+def get_coordinates(pickup_address, dropoff_address):
+
+    params = {'q': pickup_address,'limit': 3 }
+    result = requests.get(GEO_LOCALISATION, params=params).json()
+    result = result.get('features')[0]
+    coord = result.get('geometry').get('coordinates')
+
+    pickup_latitude = coord[1]
+    pickup_longitude = coord[0]
+
+    params = {'q': dropoff_address, 'limit': 3}
+
+    result = requests.get(GEO_LOCALISATION, params=params).json()
+    result = result.get('features')[0]
+    coord = result.get('geometry').get('coordinates')
+
+    dropoff_latitude = coord[1]
+    dropoff_longitude = coord[0]
+
+    return pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude
+
+def perform_predict(d, t, pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude, passenger_count):
+
+    url = urllib.parse.urljoin(TAXI_FARE_API_URL, "/predict")
+    params = {
+        'pickup_datetime': f'{d} {t}',
+        'pickup_longitude':  pickup_longitude,
+        'pickup_latitude': pickup_latitude,
+        'dropoff_longitude':  dropoff_longitude,
+        'dropoff_latitude': dropoff_latitude,
+        'passenger_count': passenger_count
     }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
 
-with one_ride:
-    #st.header("Une course")
+    result = requests.get(url, params=params).json()
+    return round(result.get("fare"), 2)
+
+def draw_map(pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude):
+    CENTER_START = [pickup_latitude, pickup_longitude]
+    m = folium.Map(location=CENTER_START, zoom_start=ZOOM_START, scrollWheelZoom=False)
+
+    folium.Marker(
+        location=[pickup_latitude, pickup_longitude],
+        tooltip="Click me!",
+        popup="Départ",
+        icon=folium.Icon(icon="cloud"),
+    ).add_to(m)
+
+    folium.Marker(
+        location=[dropoff_latitude, dropoff_longitude],
+        tooltip="Click me!",
+        popup="Destination",
+        icon=folium.Icon(color="green"),
+    ).add_to(m)
+
+
+    st_folium(
+        m,
+        center=CENTER_START,
+        zoom=ZOOM_START,
+        key="new",
+        width=1285,
+        height=725,
+        returned_objects=["all_drawings"],
+        use_container_width=True
+    )
+
+def handle_form():
 
     with st.form("prediction_form"):
 
-        d = st.date_input("C'est pour quand ?", datetime.date.today())
-        t = st.time_input("À quel heure ?", value="now")
+            d = st.date_input("C'est pour quand ?", datetime.date.today())
+            t = st.time_input("À quel heure ?", value="now")
+            pickup_address = st.text_input("Adresse de départ", '')
+            dropoff_address = st.text_input("Adresse d'arrivée", '')
+            passenger_count = st.number_input("Nombre de passager", min_value=1, max_value=7)
 
-        pickup_address = st.text_input("Adresse de départ", '')
-        dropoff_address = st.text_input("Adresse d'arrivée", '')
+            submitted = st.form_submit_button("Estimer")
 
-        passenger_count = st.number_input("Nombre de passager", min_value=1, max_value=7)
+            if submitted:
+                with st.spinner('Estimation en cours...'):
 
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Estimer")
-
-        if submitted:
-            with st.spinner('Estimation en cours...'):
-                # pickup parameters
-                params = {
-                    'q': pickup_address,
-                    'limit': 3
-                }
-                result = requests.get(GEO_LOCALISATION, params=params).json()
-                result = result.get('features')[0]
-                coord = result.get('geometry').get('coordinates')
-
-                pickup_latitude = coord[1] #-73.950655
-                pickup_longitude = coord[0] # 40.783282
-
-                CENTER_START = [pickup_latitude, pickup_longitude]
-
-                # droppoff parameters
-                params = {
-                    'q': dropoff_address,
-                    'limit': 3
-                }
-
-                result = requests.get(GEO_LOCALISATION, params=params).json()
-                result = result.get('features')[0]
-                coord = result.get('geometry').get('coordinates')
-
-                dropoff_latitude = coord[1] #-73.984365
-                dropoff_longitude = coord[0] #40.769802
-
-                url = urllib.parse.urljoin(TAXI_FARE_API_URL, "/predict")
-                params = {
-                    'pickup_datetime': f'{d} {t}',
-                    'pickup_longitude':  pickup_longitude,
-                    'pickup_latitude': pickup_latitude,
-                    'dropoff_longitude':  dropoff_longitude,
-                    'dropoff_latitude': dropoff_latitude,
-                    'passenger_count': passenger_count
-
-                }
-
-                result = requests.get(url, params=params).json()
-                amount = round(result.get("fare"), 2)
-                st.write(f'ça vous coute: :blue[{amount}$]')
-
-                m = initialize_map(center=CENTER_START, zoom=ZOOM_START)
-
-                folium.Marker(
-                    location=[pickup_latitude, pickup_longitude],
-                    tooltip="Click me!",
-                    popup="Départ",
-                    icon=folium.Icon(icon="cloud"),
-                ).add_to(m)
-
-                folium.Marker(
-                    location=[dropoff_latitude, dropoff_longitude],
-                    tooltip="Click me!",
-                    popup="Destination",
-                    icon=folium.Icon(color="green"),
-                ).add_to(m)
+                    pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude = get_coordinates(
+                        pickup_address,
+                        dropoff_address
+                    )
+                    fare = perform_predict(d, t, pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude, passenger_count)
+                    st.write(f'ça vous coute: :blue[{fare}$]')
+                    draw_map(pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude)
 
 
-                st_folium(
-                    m,
-                    center=CENTER_START,
-                    zoom=ZOOM_START,
-                    key="new",
-                    width=1285,
-                    height=725,
-                    returned_objects=["all_drawings"],
-                    use_container_width=True
-                )
+def main():
+    css_style()
+    header()
+    one_ride, many_rides = tabs()
 
+    with one_ride:
+        handle_form()
 
+    with many_rides:
+        st.write("Plusieurs courses - Dans la prochaine version 😎")
 
-
-
-with many_rides:
-    st.write("Plusieurs courses - Dans la prochaine version 😎")
+if __name__ == '__main__':
+    main()
